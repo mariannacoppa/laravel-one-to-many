c5b06776d633cab883b5736ddd9fac0dbb35e32d
mariannacoppa <densimetra@gmail.com>
2024-10-12 04:16:54 +0200

    [UPD] set show function FINE

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -5,6 +5,7 @@
     <div class="row">
         <div class="col-12">
             <h2>{{ $project->name }}</h2>
+            <h4>{{ $project->type !== null ? $project->type->name : '' }}</h4>
             <img class="project-image"
                 src="{{ $project->image !== null ? asset('./storage/'.$project->image) : 'https://placehold.co/400' }}"
                 alt="{{ $project->name }}">
