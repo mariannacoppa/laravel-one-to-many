bff814c11ca77bc9c44a4c050fe557d8d084c444
mariannacoppa <densimetra@gmail.com>
2024-10-12 03:12:13 +0200

    [UPD] error while seeding db

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Type;
 
 class TypeSeeder extends Seeder
 {
@@ -14,6 +15,14 @@ class TypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // array di tipologie
+        $types = ['Frontend', 'Backend', 'Database', 'Full Stack', 'Design'];
+
+        foreach($types as $type){
+            $new_type = new Type();
+            $new_type->name = $type;
+            $new_type->slug = Type::generateSlug($type);
+            $new_type->save();
+        }
     }
 }
